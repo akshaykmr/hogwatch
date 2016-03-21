f1b46bf50d3f1c424bd3ccecff29a93fee72067c
pikachuPokemon <akshay.kmr4321@gmail.com>
2016-03-21 20:27:26 +0530

    static files serving

--- server.py
+++ server.py
@@ -4,11 +4,20 @@ app=Bottle()
 
 #testing
 if __name__=='__main__':
-	@app.route('/')
-	@app.route('/hello')
-	@app.route('/hello/<name>')
-	def welcome(name="Stranger"):
-	    return template("Hello {{name}} !",name=name)
+	# @app.route('/')
+	# @app.route('/hello')
+	# @app.route('/hello/<name>')
+	# def welcome(name="Stranger"):
+	#     return template("Hello {{name}} !",name=name)
+
+	# @app.route('/')
+	# def landing():
+	# 	return static_file('index.html',root='')
+
+	@app.route('/<filename:path>')
+	def send_static(filename):
+	    return static_file(filename, root='./static/')
+
 
 	app.run(host='localhost', port=8080, debug=True, reloader=True)
 
--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,1 @@
-alert('hello')
+console.log('js serving');
