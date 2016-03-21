e8f68ff5d80377f2b5791cf401ccff24d3c8ed0d
pikachuPokemon <akshay.kmr4321@gmail.com>
2016-03-21 19:45:20 +0530

    static structure

--- view.py
+++ view.py
@@ -1,11 +1,11 @@
-def main(title='test_webview',location='http://google.com'):
+def test_webview(title='test_webview',location='http://google.com'):
 	import webview
 	webview.create_window(title,location);
 
 if __name__=='__main__':
 	import sys
 	if(len(sys.argv)==3):
-		main(sys.argv[1],sys.argv[2])
+		test_webview(sys.argv[1],sys.argv[2])
 	else:
 		print 'using default title and location'
-		main()
+		test_webview()
