a34502027d4c6ad47f6beadc3572910b618b470a
pikachuPokemon <akshay.kmr4321@gmail.com>
2016-03-21 20:42:47 +0530

    convert indentation to spaces and use 4 spaces from now onwards

--- server.py
+++ server.py
@@ -1,23 +1,29 @@
-from bottle import Bottle, route, run, template, static_file
+from bottle import Bottle, route, run, template, static_file, redirect
 app=Bottle()
 
 
 #testing
 if __name__=='__main__':
-	# @app.route('/')
-	# @app.route('/hello')
-	# @app.route('/hello/<name>')
-	# def welcome(name="Stranger"):
-	#     return template("Hello {{name}} !",name=name)
+    # @app.route('/')
+    # @app.route('/hello')
+    # @app.route('/hello/<name>')
+    # def welcome(name="Stranger"):
+    #     return template("Hello {{name}} !",name=name)
 
-	# @app.route('/')
-	# def landing():
-	# 	return static_file('index.html',root='')
+    # @app.route('/')
+    # def landing():
+    #   return static_file('index.html',root='')
 
-	@app.route('/<filename:path>')
-	def send_static(filename):
-	    return static_file(filename, root='./static/')
+    @app.route('/')
+    def home():
+        redirect('/index.html')
+
+    @app.route('/<filename:path>')
+    def send_static(filename):
+        return static_file(filename, root='./static/')
 
 
-	app.run(host='localhost', port=8080, debug=True, reloader=True)
+
+
+    app.run(host='localhost', port=8080, debug=True, reloader=True)
 
